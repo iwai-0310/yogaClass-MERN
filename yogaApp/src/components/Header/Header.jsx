ad9b125598852f122283bfc25cd687cb6e8ce986
iwai-0310 <chazesingh@gmail.com>
2024-02-20 23:30:53 +0530

    add sticky nature to the header

--- yogaApp/src/components/Header/Header.jsx
+++ yogaApp/src/components/Header/Header.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../../assets/app-logo.svg';
 import '../../styles/Component.css';
 
 const Header = () => {
+
+    // add state to handle sticky nature for header
+    const [sticky, setSticky] = useState(false);
+    
+    //useEffect to target scroll of 200px
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            setSticky(scrollTop >= 200);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <>
-        {/*header*/}  
-            <header className="header border-x-2 border-b-2 mb-4">
+            {/*header*/}
+            <header className={`header border-x-2 border-b-2 mb-4 ${sticky ? 'is-sticky' : ''}`}>
                 {/* left area of the header for logo */}
                 <div className="flex items-center mx-2">
                     {/* add logo image here */}
-                    <img src={logo} alt="app Logo svg" className="h-12 w-12 mx-2"  />
+                    <img src={logo} alt="app Logo svg" className="h-12 w-12 mx-2" />
                     {/* add app name here */}
                     <h1 className="text-lg font-semibold"> Brand logo</h1>
                 </div>
-            
+
                 {/* Right area of the header for explore and Login */}
                 <div className="flex items-center space-x-4 mx-2">
                     {/* add login button */}
