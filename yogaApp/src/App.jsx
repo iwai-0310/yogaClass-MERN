317a33854ba0b7bf4f7967e997dfae92736d12e4
iwai-0310 <chazesingh@gmail.com>
2024-02-20 22:18:20 +0530

    add layout to header with logo,explore and login

--- yogaApp/src/App.jsx
+++ yogaApp/src/App.jsx
@@ -7,10 +7,8 @@ function App() {
   
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Header/></>
+    <Header/>
+    </>
   )
 }
 
--- yogaApp/src/components/Header/Header.jsx
+++ yogaApp/src/components/Header/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import logo from '../../assets/app-logo.svg';
 const Header = () => {
-  return (
-    <header>
-        <h1>lets add the header here</h1>
-    </header>
-  )
-  
+    return (
+        <>
+            <header class="bg-gray-800 text-white py-2 flex justify-between items-center w-full ">
+                {/* left area of the header for logo */}
+                <div class="flex items-center mx-2">
+                    <img src={logo} alt="app Logo svg" class="h-16 w-16 mx-2"  />
+                    <h1 class="text-lg font-semibold">Your Brand</h1>
+                </div>
+            
+                {/* Right area of the header for explore and Login */}
+                <div class="flex items-center space-x-4 mx-2">
+                    <button class="bg-black hover:bg-gray-900 text-white px-4 py-2 rounded">Login</button>
+                    <button class="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded">Explore</button>
+                </div>
+            </header>
+        </>
+    )
+
 }
 
 export default Header;
