64fa24c981ec111a13db96175e84a7578c725ea0
iwai-0310 <chazesingh@gmail.com>
2024-02-21 19:12:28 +0530

    Install react-router dom

--- yogaApp/src/components/Card/cardCompact.jsx
+++ yogaApp/src/components/Card/cardCompact.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import placeHolderImg from '../../assets/placeHolderImage.jpg'
 
-const cardCompact = () => {
+const cardCompact = ({price,title,imgUrl}) => {
     return (
         <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
             <a href="" class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
                 <div class="relative pb-48 overflow-hidden">
+                     {/* use props.imgUrl to fill */}
                     <img class="absolute inset-0 h-full w-full object-cover" 
                     src={placeHolderImg}
                      alt="" />
@@ -13,11 +14,13 @@ const cardCompact = () => {
 
                 <div class="p-4 border-t border-b text-xs text-gray-700">
                     <span class="flex items-center mb-1">
-                        Price
+                        {/* use props.price */}
+                        {price}
                     </span>
                 </div>
                 <div class="p-4 flex items-center text-sm text-gray-600">
-                    <span class="ml-2">34 Bewertungen</span>
+                     {/* use props.title */}
+                    <span class="ml-2">{title}</span>
                 </div>
             </a>
         </div>
