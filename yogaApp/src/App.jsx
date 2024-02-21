fc22d77a55c08971fda7fec270b49d25a6a44788
iwai-0310 <chazesingh@gmail.com>
2024-02-21 21:31:48 +0530

    create  userContext to handle logging renders

--- yogaApp/src/App.jsx
+++ yogaApp/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import Header from '../src/components/Header/Header';
 import { Outlet } from "react-router-dom";
-
+// Import your context
+import userContext from './userContext'; 
 
 import './App.css'
 
 function App() {
-  
+  // Initialize user state
+  const [user, setUser] = useState(null);
   return (
     <div >
       <Header/> 
