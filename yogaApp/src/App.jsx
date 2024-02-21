1918056d93255f0d3eb71220096deb9fdde8c936
iwai-0310 <chazesingh@gmail.com>
2024-02-21 14:47:13 +0530

    create component Searchbanner and resize the heder

--- yogaApp/src/App.jsx
+++ yogaApp/src/App.jsx
@@ -7,12 +7,12 @@ import './App.css'
 function App() {
   
   return (
-    <>
-    <Header/>
+    <div >
+    <Header/>                                                                                              
     <Search/>
     <Search/>
     <Search/>
-    </>
+    </div>
   )
 }
 
--- yogaApp/src/components/Header/Header.jsx
+++ yogaApp/src/components/Header/Header.jsx
@@ -4,43 +4,31 @@ import '../../styles/Component.css';
 
 const Header = () => {
 
-    // add state to handle sticky nature for header
-    const [sticky, setSticky] = useState(false);
-    
-    //useEffect to target scroll of 200px
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollTop = window.scrollY;
-            setSticky(scrollTop >= 200);
-        };
+  
 
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    return (
-        <>
-            {/*header*/}
-            <header className={`header border-x-2 border-b-2 mb-4 ${sticky ? 'is-sticky' : ''}`}>
+    return (      
+            <header className={`header h-16 border-x-2 border-b-2 mb-1 w-dvh  flex justify-between items-center`}>
                 {/* left area of the header for logo */}
                 <div className="flex items-center mx-2">
                     {/* add logo image here */}
-                    <img src={logo} alt="app Logo svg" className="h-12 w-12 mx-2" />
+                    <img src={logo} alt="app Logo svg" className="h-12 w-12 mx-2   " />
                     {/* add app name here */}
-                    <h1 className="text-lg font-semibold"> Brand logo</h1>
+                    <h1 className="text-lg font-semibold  left-28"> Brand logo</h1>
                 </div>
 
                 {/* Right area of the header for explore and Login */}
-                <div className="flex items-center space-x-4 mx-2">
+                <div className="flex items-center space-x-4 mx-2 ">
                     {/* add login button */}
-                    <button className="bg-black hover:bg-gray-900 text-white px-4 py-2 rounded">Login</button>
+                    <button className="h-8 bg-black hover:bg-gray-900 text-white px-4 py-2 rounded
+                    flex items-center justify-center">
+                        <span>Login</span></button>
                     {/* add Explore button */}
-                    <button className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded">Explore</button>
+                    <button className="h-8 bg-orange-500 hover:bg-orange-600 text-white px-4 py-2
+                     rounded flex items-center justify-center">
+                        <span>Explore</span></button>
                 </div>
             </header>
-        </>
+        
     )
 
 }
