83a18fbaaa79c0e3212c2e56fec04b9e689d2bd4
iwai-0310 <chazesingh@gmail.com>
2024-02-21 20:44:54 +0530

    adds api psuedo endpoint and map card component with data

--- yogaApp/src/pages/home.jsx
+++ yogaApp/src/pages/home.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from '../components/SearchSection/Search';
 import Cardcc from '../components/Card/cardCompact'
 
+const apiForCardData='';
+
 const home = () => {
+  // use State for fill the card 
+  const [cardsData, setCardsData] = useState([]);
+
+  //use Effect to set card data
+  useEffect(() => {
+    // Fetch data from your API
+    fetch(apiForCardData)
+      .then((response) => response.json())
+      .then((data) => setCardsData(data));
+  }, []);
+
   return (
     <div>
         <Search/>
-        <Cardcc/>
+        {/* Map the card with data from state(cardsData) */}
+        {cardsData.map((card) => (
+        <Cardcc
+          key={card.id}
+          title={card.title}
+          price={card.price}
+          imgurl={card.imgUrl} 
+        />
+      ))}
     </div>
   )
 }
