788b471d3f301cae3c4a2b27ea908fcd6af25bb7
iwai-0310 <chazesingh@gmail.com>
2024-02-21 15:44:51 +0530

    createSearch box layout with input and button

--- yogaApp/src/App.jsx
+++ yogaApp/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from '../src/components/Header/Header';
 import Search from '../src/components/SearchSection/Search';
+import SearchBox from '../src/components/SearchSection/SearchBox';
 
 import './App.css'
 
@@ -8,7 +9,8 @@ function App() {
   
   return (
     <div >
-    <Header/>                                                                                              
+    <Header/> 
+    <SearchBox/>                                                                                      
     <Search/>
     </div>
   )
