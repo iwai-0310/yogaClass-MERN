023f17afc812169fc203afc48af78d8d583a9004
iwai-0310 <chazesingh@gmail.com>
2024-02-21 22:20:39 +0530

    test- API / route SUCCESS

--- yogaApp/Backend/server.js
+++ yogaApp/Backend/server.js
@@ -2,6 +2,12 @@ const express = require('express')
 // create app variable as express instant
 const app = express()
 
+//routes here
+app.get('/', (req, res) => {
+    //send greetings to Client
+    res.send('Hello NODE API')
+})
+
 //isten to port 3000
 app.listen(3000,()=>{
     console.log('Node API running on port 3000');
