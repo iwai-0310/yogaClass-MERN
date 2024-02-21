f0bbaeb0a7a80a4df5e788f484050732283abee3
iwai-0310 <chazesingh@gmail.com>
2024-02-22 00:25:48 +0530

    create a json file containing data for database

---
+++ yogaApp/Backend/cardsData.js
@@ -0,0 +1,145 @@
+[
+    {
+        "title": "100-Days Weight Loss Program for everyone- Hindi- 1 year",
+        "price": 2999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/WeightLossProductBanner.jpg?v=1706333905"
+    },
+    {
+        "title": "20 LIFESTYLE AILMENTS - HEALING WITH YOGA",
+        "price": 1999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/20-LIFESTYLE-AILMENTS---HEALING-WITH-YOGA.jpg?v=1704263466"
+    },
+    {
+        "title": "20-Hrs pranayama for Holistic Wellness Daily Practice",
+        "price": 1999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thubmnails1.png?v=1706359181"
+    },
+    // 3 done
+    {
+        "title": "20-Yoga Therapies for Lifestyle Diseases",
+        "price": 999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails_16c881f0-7f66-46c1-85ad-4ec06f8cc96c.jpg?v=1706353171"
+    },
+    {
+        "title": "30 Days Weight Loss Essential with yoga",
+        "price": 999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/WeightEssentialwithyoga.jpg?v=1706359326"
+    },
+    {
+        "title": "30 Days yoga For Beginner",
+        "price": 499,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/WhatsAppImage2024-01-13at7.19.19PM.jpg?v=1705325399"
+    },
+    {
+        "title": "30- Days Pranayama for Holistic Wellness",
+        "price": 699,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails.png?v=1705406276"
+    },
+    // 7 done
+    {
+        "title": "50 Hours Pranayama (breathwork) English",
+        "price": 2999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/50-hrs-ProductBanner.jpg?v=1706359225"
+    },
+    {
+        "title": "50 Hours Yoga Foundation",
+        "price": 1999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/50-Hours-Yoga-Foundation.jpg?v=1706359257"
+    },
+    {
+        "title": "Certificate in Chakra healing – 7 Chakra healing",
+        "price": 9999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails_4f25c44d-13fe-4110-bc36-137c43bd3c2c.jpg?v=1706359870"
+    },
+    {
+        "title": "Chronic Pain Management With Yoga",
+        "price": 699,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails.jpg?v=1705381971"
+    },
+    // 11 done
+    {
+        "title": "Daily Yoga For Everyone",
+        "price": 1999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails_2031e935-ac1f-4eab-9e3e-be42bb023995.jpg?v=1706346714"
+    },
+    {
+        "title": "Foundation 2-in-1 Program Bundle",
+        "price": 1099,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/WhatsAppImage2024-01-16at2.53.24PM_1.jpg?v=1705398962"
+    },
+    {
+        "title": "Foundation 2-in-1 Program Bundle",
+        "price": 1099,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/Artboard2_2.jpg?v=1705414144"
+    },
+    {
+        "title": "Holistic Wellness with 30 Yoga Guru in 30 Days",
+        "price": 1999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/with-30-Yoga-Guru-in-30-Days.jpg?v=1704287947"
+    },
+    //15 done
+    {
+        "title": "Pranayama 2-in-1 Program Bundle",
+        "price": 1799,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/Artboard1_1.jpg?v=1705414076"
+    },
+    {
+        "title": "Ultimate Yog Mastery 1-Year Master Course",
+        "price": 4999,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/About-Image-2_32d025ef-9638-4e43-93a4-aed48a5d1931.jpg?v=1706358328"
+    },
+    {
+        "title": "Weight Loss 2-in-1 Program Bundle",
+        "price": 1399,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/Artboard3_2.jpg?v=1705414180"
+    },
+    {
+        "title": "Yoga For Depression And Stress",
+        "price": 699,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/Yoga-for-Depression-and-Stress.webp?v=1706098053"
+    },
+    {
+        "title": "Yoga for Diabetes Daily Practice 30 Minutes",
+        "price": 699,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails_ee5cca3b-0407-401c-92d4-98523389bb75.jpg?v=1706092192"
+    },
+    //20 done
+    {
+        "title": "Yoga for Hypertension and Cardio",
+        "price": 1399,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/Yoga-for-Hypertension-and-Cardio.webp?v=1706357502"
+    },
+    {
+        "title": "Yoga For Mental Health",
+        "price": 699,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails_b6b3eb13-fb49-4a19-9a78-246c9b4155a2.jpg?v=1704522883"
+    },
+    {
+        "title": "Yoga For Respiratory System And Immunity Boosting",
+        "price": 699,
+        "live": false,
+        "image": "https://cdn.shopify.com/s/files/1/0676/7394/8419/files/thumbnails3.jpg?v=1706101893"
+    },
+]
