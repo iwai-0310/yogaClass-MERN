a697206f4a0bd6c6490b58b17bff9701f9273c83
iwai-0310 <chazesingh@gmail.com>
2024-02-22 13:36:49 +0530

    call the api and store data into state

--- yogaApp/src/pages/CardBookingView.jsx
+++ yogaApp/src/pages/CardBookingView.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { useParams } from "react-router-dom";
-const CardBookingView = () => {
 
+//api endpoint to access all data
+const apiUrl= 'http://localhost:3000/api/v1/cards';
+const CardBookingView = () => {
+    //set state to save card clicked details
+    const [data, setData] = useState([]);
     //access the product id
-//   const { id } = useParams();
+    const { id } = useParams();
+
+    //access all the products data
+  //now try and fetch all the data
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const apiData = data.cards;
+        console.log("this default data is apiData is", typeof apiData);
+        console.log("and the value of the apiData is", apiData);
+        setData(apiData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
     <div><h1>this </h1></div>
   )
