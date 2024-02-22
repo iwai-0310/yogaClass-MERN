27c8dc0421a1d1b1642c79d47c6bd55361f9359e
iwai-0310 <chazesingh@gmail.com>
2024-02-22 14:00:48 +0530

    add calendar dependeecy to project

--- yogaApp/src/pages/CardBookingView.jsx
+++ yogaApp/src/pages/CardBookingView.jsx
@@ -1,39 +1,61 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 
 //api endpoint to access all data
-const apiUrl= 'http://localhost:3000/api/v1/cards';
+const apiUrl = 'http://localhost:3000/api/v1/cards';
 const CardBookingView = () => {
     //set state to save card clicked details
     const [data, setData] = useState([]);
-    //access the product id
+    //access the card id
     const { id } = useParams();
 
-    //access all the products data
-  //now try and fetch all the data
-  useEffect(() => {
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const apiData = data.cards;
-        console.log("this default data is apiData is", typeof apiData);
-        console.log("and the value of the apiData is", apiData);
-        setData(apiData);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    //access all the pcard data
+    //now try and fetch all the data
+    useEffect(() => {
+        fetch(apiUrl)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                const apiData = data.cards;
+                console.log("this default data is apiData is", typeof apiData);
+                console.log("and the value of the apiData is", apiData);
+                setData(apiData);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
+    const result = data.length > 0 ? data.filter((p) => p._id === id) : [];
 
-  return (
-    <div><h1>this </h1></div>
-  )
+    return (
+        <div className='w-dvh h-dvh'>
+            {result.map((item) => (
+                
+
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+                    <a href="#">
+                        <img class="rounded-t-lg" src={item.image} alt={item.name} />
+                    </a>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 flex items-center justify-between">
+                            <span>{item.price}</span>
+                            <span>{item.time}</span>
+                        </p>
+                        
+                    </div>
+                </div>
+
+            ))}
+        </div>
+    )
 }
 
 export default CardBookingView
